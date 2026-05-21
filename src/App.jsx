296d96ef82e7b1e6a0c463646150dc27111320b9
aliishonu-design <aliishonu@gmail.com>
2026-05-21 20:50:13 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -923,7 +923,7 @@ function LoginScreen() {
           <a href="https://sakumemo-1.vercel.app/privacy-policy.html" target="_blank" style={{color:G}}>プライバシーポリシー</a>・
           <a href="https://sakumemo-1.vercel.app/terms-of-service.html" target="_blank" style={{color:G}}>利用規約</a>
         </div>
-        <div style={{fontSize:".62rem",color:"#ccc",marginTop:8}}>v1.4.8</div>
+        <div style={{fontSize:".62rem",color:"#ccc",marginTop:8}}>v1.5.4</div>
       </div>
     </div>
   );
